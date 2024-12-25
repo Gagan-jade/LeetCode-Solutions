15aec8be53084b8a3d1ca1f04c7369ee7b4124ce
Gagan-jade <gagandeepnaidul@gmail.com>
2024-12-25 23:44:29 +0530

    Create answer.c

---
+++ answer.c
@@ -0,0 +1,28 @@
+typedef struct ListNode* NODE;
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+    if (head == NULL)
+        return head;
+    if (head->next == NULL)
+        return head;
+    if (k == 0)
+        return head;
+    NODE cur,prev;
+    cur = head;
+    int n=0;
+    while(cur!=NULL){
+        cur = cur->next;
+        n++;
+    }
+    k = k%n;
+    cur = head;
+    for(int i=0;i<k;i++){
+        while(cur->next!=NULL){
+            prev = cur;
+            cur = cur->next;
+        }
+        prev->next = NULL;
+        cur->next = head;
+        head = cur;
+    }
+    return head;
+}
